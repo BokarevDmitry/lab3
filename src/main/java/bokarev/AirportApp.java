0418dc17d09fb919e492c9dc3b0920939de6bb4b
BokarevDmitry <dbokarevv@gmail.com>
2019-10-23 13:22:12 +0300

    Scripted auto-commit on change (2019-10-23 13:22:12) by gitwatch.sh

--- src/main/java/bokarev/AirportApp.java
+++ src/main/java/bokarev/AirportApp.java
@@ -1,9 +1,13 @@
 package bokarev;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
 public class AirportApp {
     SparkConf conf = new SparkConf().setAppName("AirportApp");
     JavaSparkContext sc = new JavaSparkContext(conf);
+
+    JavaRDD<String> flightsData = sc.textFile("/src/main/resources/664600583_T_ONTIME_sample.csv");
+    JavaRDD<String> airportsData = sc.textFile("/src/main/resources/L_AIRPORT_ID.csv");
 
 }
