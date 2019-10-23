87a005bb3e0fd30e77ccee0540dec4b0f7ab0e09
BokarevDmitry <dbokarevv@gmail.com>
2019-10-23 15:05:15 +0300

    Scripted auto-commit on change (2019-10-23 15:05:15) by gitwatch.sh

--- src/main/java/bokarev/AirportMapper.java
+++ src/main/java/bokarev/AirportMapper.java
@@ -1,4 +1,21 @@
 package bokarev;
 
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
 public class AirportMapper {
+    private static String DESCRIPTION_LINE = "Code";
+
+    //@Override
+    protected void map(LongWritable key, Text value, Mapper.Context context) throws IOException, InterruptedException {
+        String[] columns = CSVParser.parseAirports(value);
+        String airportCode = CSVParser.getAirCode(columns);
+
+        if (!airportCode.contains(DESCRIPTION_LINE)) {
+            String airportName = CSVParser.getAirportName(columns);
+        }
+    }
 }
--- src/main/java/bokarev/CSVParser.java
+++ src/main/java/bokarev/CSVParser.java
@@ -1,4 +1,19 @@
 package bokarev;
+import org.apache.hadoop.io.Text;
 
 public class CSVParser {
+    public static String[] parseFlights(Text s) {
+        return s.toString().split(",");
+    }
+    public static String[] parseAirports (Text s) {
+        return s.toString().split("\",\"");
+    }
+
+    public static String removeQuotes (String s) {return s.replaceAll("[\"]", "");}
+
+    public static String getAirCode (String[] s) {return s[0];}
+    public static String getAirportName (String[] s) { return s[1];}
+
+    public static String getAirportCode (String[] s) {return s[14];}
+    public static String getDelayTime (String[] s) {return s[18];}
 }
