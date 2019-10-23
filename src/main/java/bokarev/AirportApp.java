7e0c067405bafa6772f4fa4c60d83b00aa0830d0
BokarevDmitry <dbokarevv@gmail.com>
2019-10-23 15:32:47 +0300

    Scripted auto-commit on change (2019-10-23 15:32:47) by gitwatch.sh

--- src/main/java/bokarev/AirportApp.java
+++ src/main/java/bokarev/AirportApp.java
@@ -9,6 +9,8 @@ import java.util.List;
 
 public class AirportApp {
     public static void main(String[] args) throws Exception {
+        String DESCRIPTION_LINE = "ARR_DELAY_NEW";
+
         SparkConf conf = new SparkConf().setAppName("AirportApp");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
@@ -20,7 +22,7 @@ public class AirportApp {
                 (String s) -> {
                     String[] columns = s.split(",");
                     String airportsPair = columns[11] + "-" + columns[14];
-                    if (!columns[18].contains("ARR")) {
+                    if (!columns[18].contains(DESCRIPTION_LINE) && !columns[18].isEmpty() && Float.parseFloat(columns[18])>0) {
                         Float timeDelay = Float.parseFloat(columns[18]);
                         //String cancelStatus = columns[19];
                         return new Tuple2<>(airportsPair, timeDelay);
