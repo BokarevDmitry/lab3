fe6037d0fa555b2d9a5f7abbff7e4859668c18bf
BokarevDmitry <dbokarevv@gmail.com>
2019-10-25 02:07:16 +0300

    Scripted auto-commit on change (2019-10-25 02:07:15) by gitwatch.sh

--- src/main/java/bokarev/floatPair.java
+++ src/main/java/bokarev/floatPair.java
@@ -16,7 +16,11 @@ public class floatPair implements Serializable {
         this.countRecords = 1;
         if (timeDelay>=1.0 || cancelStatus==1.0) {
             this.countDelayOrCancel = 1;
-        } else this.countDelayOrCancel = 0;
+            System.out.println("DELAY1");
+        } else {
+            this.countDelayOrCancel = 0;
+            System.out.println("DELAY0");
+        }
     }
 
     public floatPair(Float timeDelay, Integer countRecords, Integer countDelayOrCancel) {
