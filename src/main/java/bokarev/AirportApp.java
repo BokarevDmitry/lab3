7d3fcf5aa7a29657c00c84954a8f2a9884101a2c
BokarevDmitry <dbokarevv@gmail.com>
2019-10-25 23:14:45 +0300

    Scripted auto-commit on change (2019-10-25 23:14:45) by gitwatch.sh

--- src/main/java/bokarev/AirportApp.java
+++ src/main/java/bokarev/AirportApp.java
@@ -15,8 +15,7 @@ public class AirportApp {
 
         JavaRDD<String> flightsRDD = sc.textFile("/user/dima/664600583_T_ONTIME_sample.csv");
         JavaRDD<String> airportsRDD = sc.textFile("/user/dima/L_AIRPORT_ID.csv");
-
-        //flightsRDD.map(AirportApp.map);
+        
         JavaPairRDD<Tuple2, floatPair> pairs = flightsRDD.mapToPair(
                 (String s) -> {
                     String[] flightsInfo = CSVParser.parseFlights(s);
@@ -45,23 +44,15 @@ public class AirportApp {
 
         JavaPairRDD<Tuple2, floatPair> maxDelayTime = pairs.reduceByKey(
                 (floatPair a, floatPair b) -> {
-                    //a.countDelayOrCancel += b.countDelayOrCancel;
-                    //a.countRecords+=1;
-                    //return Math.max(a,b);
                     return new floatPair( Math.max(a.getTimeDelay(), b.getTimeDelay()),
                             a.countRecords+b.countRecords,
                             a.countDelayOrCancel + b.countDelayOrCancel);
                 }
         );
 
-        //System.out.println("LGSDHGKJDHFLDFDSLFDF");
-        //Tuple2<Tuple2, floatPair> el = maxDelayTime.collect().get(0);
-        //System.out.println(el._1+ " "+ el._2.getCountRecords()+ " "+ el._2.getCountDelayOrCancel());
-
 
         JavaPairRDD<Tuple2, maxAndPercentPair> percentPairs = maxDelayTime.mapToPair(
                 (Tuple2<Tuple2, floatPair> a) -> {
-                    //System.out.println(a._1+ " "+ a._2.getCountRecords()+ " "+ a._2.getCountDelayOrCancel());
                     return new Tuple2<>(a._1, new maxAndPercentPair(a._2.getTimeDelay(), a._2.getCountRecords(), a._2.getCountDelayOrCancel()));
                 }
         );
@@ -70,19 +61,7 @@ public class AirportApp {
                 (Tuple2<Tuple2, maxAndPercentPair> a) -> new Tuple2<>(a._1, new Tuple2<>(a._2.maxDelay, a._2.percent))
         );
 
-        JavaPairRDD<Tuple2, Tuple2> last2 = maxDelayTime.mapToPair(
-                (Tuple2<Tuple2, floatPair> a) -> new Tuple2<>(a._1, new Tuple2<>(a._2.getCountRecords(), a._2.getCountDelayOrCancel()))
-        );
-
-        JavaPairRDD<Tuple2, Tuple2> lastPairs = pairs.mapToPair(
-                (Tuple2<Tuple2, floatPair> a) -> new Tuple2<>(a._1, new Tuple2<>(a._2.getCountRecords(), a._2.getCountDelayOrCancel()))
-        );
-
         System.out.println(last.collect());
 
-
-        //List<String> flightsList = flightsRDD.collect();
-        //List<String> airportList = airportsRDD.collect();
-        //System.out.println(s.get(5));
     }
 }
